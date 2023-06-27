996e6eefabeb5a77814cf7da91642c6231920f98
= <=>
2023-06-27 19:51:49 +0800

    3

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -81,13 +81,18 @@ class ResNet(nn.Module):
         out = self.avg_pool(out)
         out = out.view(out.size(0), -1)
         return out
-    
+
 
 def read_data():
     # 这里可自行修改数据预处理，batch大小也可自行调整
+    transform = torchvision.transforms.Compose([
+    torchvision.transforms.Pad(4),
+    torchvision.transforms.RandomHorizontalFlip(),
+    torchvision.transforms.RandomCrop(32),
+    torchvision.transforms.ToTensor()])
     # 保持本地训练的数据读取和这里一致
-    dataset_train = torchvision.datasets.CIFAR10(root='../data/exp03', train=True, download=True, transform=torchvision.transforms.ToTensor())
-    dataset_val = torchvision.datasets.CIFAR10(root='../data/exp03', train=False, download=False, transform=torchvision.transforms.ToTensor())
+    dataset_train = torchvision.datasets.CIFAR10(root='../data/exp03', train=True, download=True, transform=transform)
+    dataset_val = torchvision.datasets.CIFAR10(root='../data/exp03', train=False, download=False, transform=transform)
     data_loader_train = DataLoader(dataset=dataset_train, batch_size=50, shuffle=True)
     data_loader_val = DataLoader(dataset=dataset_val, batch_size=50, shuffle=False)
     return dataset_train, dataset_val, data_loader_train, data_loader_val
@@ -96,6 +101,6 @@ def main():
     model = ResNet(ResBlock, [3, 4, 6, 3]) # 若有参数则传入参数 ResNet-34
     current_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(current_dir)
-    model.load_state_dict(torch.load(parent_dir + '/pth/model.pth'))
+    model.load_state_dict(torch.load(parent_dir + '/pth/model.pth', map_location=torch.device('cpu')))
     return model
     
